13177cc45fcc9a8880c59ac3582c6fae43ef4734
suchao <822075222@qq.com>
2018-12-03 15:53:47 +0800

    spring data jpa 分页查询

--- spring-boot-jpa/src/main/java/com/suchaos/model/User.java
+++ spring-boot-jpa/src/main/java/com/suchaos/model/User.java
@@ -2,6 +2,7 @@ package com.suchaos.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,6 +18,7 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@ToString
 public class User {
 
     @Id
--- spring-boot-jpa/src/test/java/com/suchaos/repository/UserRepositoryTest.java
+++ spring-boot-jpa/src/test/java/com/suchaos/repository/UserRepositoryTest.java
@@ -2,18 +2,19 @@ package com.suchaos.repository;
 
 import com.suchaos.model.User;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import java.text.DateFormat;
-import java.util.Date;
-
-import static org.junit.Assert.*;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.startsWith;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,26 +23,60 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Before
+    public void setUp() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH-mm-ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String regTime = dateTimeFormatter.format(localDateTime);
+
+        for (int i = 1; i <= 10; i++) {
+            userRepository.save(new User(String.valueOf(i), "password" + i, i + "@gmail.com", i + "nickname", regTime));
+        }
+    }
+
     @Test
     public void test() {
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
-        String formattedDate = dateFormat.format(date);
-
-        /*userRepository.save(new User("aa", "aa@126.com", "aa", "aa123456", formattedDate));
-        userRepository.save(new User("bb", "bb@126.com", "bb", "bb123456", formattedDate));
-        userRepository.save(new User("cc", "cc@126.com", "cc", "cc123456", formattedDate));*/
-
-        //Assert.assertEquals(3, userRepository.findAll().size());
-        Assert.assertEquals("bb123456", userRepository.findByUserNameOrEmail("bb", "cc@126.com").getNickName());
+        Assert.assertEquals(10, userRepository.findAll().size());
         //userRepository.delete(userRepository.findByUserName("aa"));
     }
 
     @Test
     public void findByUserName() {
+        Assert.assertEquals("2nickname", userRepository.findByUserName("2").getNickName());
     }
 
     @Test
     public void findByUserNameOrEmail() {
+        Assert.assertEquals("1nickname", userRepository.findByUserNameOrEmail("1", null).getNickName());
+    }
+
+    @Test
+    public void findAllPagealbe() {
+        int page = 2, size = 2;
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<User> users = userRepository.findAll(pageable);
+        printPage(users);
+    }
+
+    @Test
+    public void findByExample() {
+        int page = 2, size = 2;
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        User user = new User();
+        user.setPassWord("password1");
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withMatcher("passWord", startsWith().ignoreCase());
+        Example<User> example = Example.of(user, matcher);
+        Page<User> userPage = userRepository.findAll(example, pageable);
+        printPage(userPage);
+    }
+
+    private static void printPage(Page<User> userPage) {
+        System.out.println("total pages: " + userPage.getTotalPages());
+        System.out.println("total elements: " + userPage.getTotalElements());
+        userPage.forEach(System.out::println);
     }
 }
