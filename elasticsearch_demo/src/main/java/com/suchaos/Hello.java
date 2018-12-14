bfbe94b9c304ba4704544c1c6ee2da0cdde93b90
suchao <822075222@qq.com>
2018-12-14 14:16:56 +0800

    elasticsearch java api 实验

---
+++ elasticsearch_demo/src/main/java/com/suchaos/Hello.java
@@ -0,0 +1,13 @@
+package com.suchaos;
+
+/**
+ * TODO description class
+ *
+ * @author suchao
+ * @date 2018/12/14
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
