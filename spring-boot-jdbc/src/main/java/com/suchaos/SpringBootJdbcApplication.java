c9ddcc589d2436c835bab4db66f5793778a2e573
suchao <suchao193@gmail.com>
2019-03-22 22:49:54 +0800

    springboot 单数据源

--- spring-boot-jdbc/src/main/java/com/suchaos/SpringBootJdbcApplication.java
+++ spring-boot-jdbc/src/main/java/com/suchaos/SpringBootJdbcApplication.java
@@ -1,12 +1,48 @@
 package com.suchaos;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class SpringBootJdbcApplication {
+@Slf4j
+public class SpringBootJdbcApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJdbcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() {
+        log.info("dataSource: " + dataSource.toString());
+        try (Connection connection = dataSource.getConnection()) {
+            log.info("connection: " + connection.toString());
+        } catch (SQLException e) {
+            log.error("sql error", e);
+        }
+
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM FOO")
+                .forEach(row -> log.info(row.toString()));
     }
 }
